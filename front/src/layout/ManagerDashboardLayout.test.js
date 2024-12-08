0e24a7647d765b97f66bafd7d2cfa2c1c45867c2
rradic <radovan.radic@student.um.si>
2024-12-09 00:05:30 +0100

    Added  await

--- front/src/layout/ManagerDashboardLayout.test.js
+++ front/src/layout/ManagerDashboardLayout.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import ManagerDashboardLayout from '../layout/ManagerDashboardLayout';
 
 describe('ManagerDashboardLayout Component', () => {
-    test('renders Manager Dashboard title', () => {
-        render(<ManagerDashboardLayout />);
-        expect(screen.getByText('Manager Dashboard')).toBeInTheDocument();
+    test('renders Manager Dashboard title', async () => {
+        render(<ManagerDashboardLayout/>);
+        await waitFor(
+            expect(screen.getByText('Manager Dashboard')).toBeInTheDocument());
     });
 });
