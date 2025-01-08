5a85648ac6ca4e8472a7a41986a59b7f1594cb3a
rradic <radovan.radic@student.um.si>
2025-01-08 23:33:23 +0100

    Env

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://octopus-app-tzifw.ondigitalocean.app/api",
   headers: {
     "Content-Type": "application/json"
   },
@@ -26,7 +26,7 @@ api.interceptors.request.use(
 );
 
 const apiCsv = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://octopus-app-tzifw.ondigitalocean.app/api',
   headers: {
     'Accept': 'text/octet-stream',
     'Content-Type': 'text/octet-stream',
